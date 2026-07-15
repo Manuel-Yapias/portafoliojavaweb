c0cb5f4dcdbc7718a65715548c518e48841a52bd
Manuel-Yapias <manuelyapiasr61@gmail.com>
2026-07-15 02:47:43 -0500

    Delete src/main directory

--- src/main/java/com/mycompany/folderrosalesd3/model/S01E06Model.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.folderrosalesd3.model;
-
-public class S01E06Model {
-    private int n1, n2;
-    public S01E06Model(int n1, int n2) { this.n1 = n1; this.n2 = n2; }
-    public int getCociente() { return n1 / n2; }
-    public int getResiduo() { return n1 % n2; }
-}
