47b04f3266199e0c1233265fa3ff4ce094d80b67
Manuel-Yapias <manuelyapiasr61@gmail.com>
2026-07-16 14:37:46 -0500

    Add files via upload

---
+++ src/main/java/com/mycompany/folderrosalesd3/model/semana11/S11E1Model.java
@@ -0,0 +1,19 @@
+package com.mycompany.folderrosalesd3.model.semana11;
+
+public class S11E1Model {
+    private String titulo;
+    private String descripcionGeneral;
+
+    public S11E1Model() {
+        this.titulo = "Manual de Evolución Visual: Del Naipe al Código";
+        this.descripcionGeneral = "Análisis retrospectivo sobre la adaptación y simplificación de interfaces físicas de póker hacia un entorno web responsivo.";
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDescripcionGeneral() {
+        return descripcionGeneral;
+    }
+}
