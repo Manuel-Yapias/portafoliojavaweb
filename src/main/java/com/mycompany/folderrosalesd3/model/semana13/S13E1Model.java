521b41c6ede783196e3f273784a60b9a264c5ca0
Manuel-Yapias <manuelyapiasr61@gmail.com>
2026-07-17 10:32:59 -0500

    Add files via upload

---
+++ src/main/java/com/mycompany/folderrosalesd3/model/semana13/S13E1Model.java
@@ -0,0 +1,17 @@
+package com.mycompany.folderrosalesd3.model.semana13;
+
+public class S13E1Model {
+    private String estudiante;
+    private String curso;
+    private String cicloSeccion;
+
+    public S13E1Model() {
+        this.estudiante = "ROSALES YAPIAS MANUEL";
+        this.curso = "LENGUAJE DE PROGRAMACIÓN";
+        this.cicloSeccion = "III - D";
+    }
+
+    public String getEstudiante() { return estudiante; }
+    public String getCurso() { return curso; }
+    public String getCicloSeccion() { return cicloSeccion; }
+}
